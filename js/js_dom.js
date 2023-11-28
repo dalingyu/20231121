681883e50822efb2c2ce9d502069789fe5f8f9ff
dalingyu <dnps912@gmail.com>
2023-11-28 22:06:06 +0800

    js

---
+++ js/js_dom.js
@@ -0,0 +1,16 @@
+let uid = document.querySelector('#uid');
+let student = document.querySelector('.student');
+
+console.log(uid);
+
+uid.innerHTML = 'New UID';
+
+console.log(student);
+
+let uids = document.querySelectorAll('#uid');
+let students = document.querySelectorAll('.student');
+console.log(uids);
+console.log(students);
+
+
+let fullName = document.querySelector('#full-name');
